b2a2bef5ce48c041fb85863bb8961d2905e808c7
Ilya Safonov <safonov.ilya@gmail.com>
2016-02-21 13:49:09 +0500

    wiggle-sort-II

--- wiggle-sort-II/index.js
+++ wiggle-sort-II/index.js
@@ -39,7 +39,7 @@ var wiggleSort = function(nums) {
 
     // 2 3 3 4 -> 3 4 2 3
     for (var i = (nums.length - nums.length % 2)- 2; i > 0; i -= 2, count++) {
-        if (nums[i] == nums[i - 1]) {
+        if (nums[i] >= nums[i - 1]) {
             // 4 5 5 6 5 7 -> 5 7 5 6 4 5
             //     i   j
             var j = i;
@@ -62,52 +62,80 @@ function swap(arr, i , j){
     arr[j] = tmp;
 }
 
+function isValidWiddleSort(arr) {
+    var i=0;
+    while(i < arr.length-1){
+        if (i%2){
+            if (arr[i] <= arr[i+1]){
+                return false
+            }
+        } else {
+            if (arr[i] >= arr[i+1]){
+                return false
+            }
+        }
+        i++;
+    }
+    return true;
+}
+
+describe('isValidWiddleSort', function () {
+    it('should return true', function(){
+        assert.ok(isValidWiddleSort([1,2,1,3,2]), 'simple arr')
+    })
+    it('should return false', function(){
+        assert.ok(!isValidWiddleSort([3,2]));
+    })
+    it('should return false, duplicates', function(){
+        assert.ok(!isValidWiddleSort([1,2,2]));
+    })
+})
+
 describe('wiggleSort', function () {
     it('should return array with pairs', function () {
         var nums = [1, 2, 3, 4];
         wiggleSort(nums);
-        assert.deepEqual([1, 2, 3, 4], nums);
+        assert.ok(isValidWiddleSort(nums));
     });
     it('should return array with MinMax pairs', function () {
         var nums = [2, 1, 3, 4];
         wiggleSort(nums);
-        assert.deepEqual([1, 2, 3, 4], nums);
+        assert.ok(isValidWiddleSort(nums));
     });
     it('array with repeated elements should return MinMax pairs', function () {
         var nums = [2, 2, 3, 4];
         wiggleSort(nums);
-        assert.deepEqual([2, 3, 2, 4], nums);
+        assert.ok(isValidWiddleSort(nums));
     });
     it('should return push MinMax pairs with eq neibers to top of list', function () {
         var nums = [2, 3, 3, 4];
         wiggleSort(nums);
-        assert.deepEqual([3, 4, 2, 3], nums);
+        assert.ok(isValidWiddleSort(nums));
     });
     it('should return push MinMax pairs with eq neibers to top of list', function () {
         var nums = [1, 3, 2, 2, 2, 1, 1, 3, 1, 1, 2, 3];
         wiggleSort(nums);
-        assert.deepEqual([ 1, 3, 2, 3, 1, 2, 1, 2, 1, 2, 1, 3 ], nums);
+        assert.ok(isValidWiddleSort(nums));
     });
     it('not even nums', function () {
         var nums = [1, 3, 2, 2, 2, 1, 1, 3, 1, 1, 2];
         wiggleSort(nums);
-        assert.deepEqual([ 1, 3, 2, 3, 1, 2, 1, 2, 1, 2, 1 ], nums);
+        assert.ok(isValidWiddleSort(nums));
     });
     it('after pairs creat last reapeted', function () {
         var nums = [2, 1, 1, 2, 1, 3, 3, 3, 1, 3, 1, 3, 2];
                  //[1, 2, 1, 2, 1, 3, 1, 3, 1, 3, 2, 3, 3]
         wiggleSort(nums);
-        assert.deepEqual([1, 3, 1, 2, 1, 3, 1, 3, 1, 3, 2, 3, 2], nums);
+        assert.ok(isValidWiddleSort(nums));
     });
     it('after pairs creat last reapeted', function () {
         var nums = [3, 2, 3, 3, 2, 1, 1, 2, 3, 1, 1, 3, 2, 1, 2, 2, 2, 2, 1];
-                 //[2, 3, 2, 3, 1, 3, 1, 2, 1, 3, 1, 3, 1, 2, 1, 2, 2, 2, 2]
         wiggleSort(nums);
-        assert.deepEqual([1, 3, 1, 2, 1, 3, 1, 3, 1, 3, 2, 3, 2], nums);
+        assert.ok(isValidWiddleSort(nums));
     });
     it('should return push MinMax pairs with eq neibers smart swap', function () {
         var nums = [4, 5, 5, 5, 5, 6, 6, 6];
         wiggleSort(nums);
-        assert.deepEqual([5, 6, 5, 6, 5, 6, 4, 5], nums);
+        assert.ok(isValidWiddleSort(nums));
     });
 })
